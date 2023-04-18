4063d4d452e90a2b664731f77a072e295a43fcd6
Ary Okoli <aco.umeadi@gmail.com>
2023-04-18 10:36:43 -0400

    simple liker app lab complete

--- main.js
+++ main.js
@@ -2,7 +2,47 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+userLikes = document.querySelectorAll(".like-glyph")
+//userLikes.forEach((el) => {
+//  el.addEventListener("click", updateHeart)
+//})
+
+for(const el of userLikes ) {
+  el.addEventListener("click", updateHeart)
+}
+
+function updateHeart(event) {
+    const hidden = document.querySelector("#modal")
+    let response = mimicServerCall()
+    // switch (response) { 
+    // case (event.target === EMPTY_HEART):
+    if (event.target.innerHTML === EMPTY_HEART) {
+    response.then((success) => {
+        event.target.classList.add("activated-heart")
+        event.target.textContent = FULL_HEART
+    }).catch((error) => {
+        hidden.classList.remove("hidden")
+        document.querySelector("#modal-message").textContent = error.message
+        setTimeout((error) => hidden.classList.add("hidden"), 3000)
+    })
+    // break
+    } else {
+    //case (event.target === FULL_HEART):
+    //  event.target.innerHTML = FULL_HEART
+      response.then((success) => {
+        event.target.classList.remove("activated-heart")
+        event.target.textContent = EMPTY_HEART
+    }).catch((error) => {
+        hidden.classList.remove("hidden")
+        document.querySelector("#modal-message").textContent = error.message
+        setTimeout((error) => hidden.classList.add("hidden"), 3000)
+    })
+    //break
+    }
+    
+}
 
 
 
